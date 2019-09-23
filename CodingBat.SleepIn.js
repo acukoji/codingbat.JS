5930569afcb468f2f188dfd8ebdd02b028199fcf
Acukoji <acukoji@gmail.com>
2019-09-22 22:15:30 -0600

    Solution to SleepIn

---
+++ CodingBat.SleepIn.js
@@ -0,0 +1,40 @@
+
+/*The parameter weekday is true if it is a weekday, 
+and the parameter vacation is true if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+Examples
+
+sleepIn(true, true) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+
+/* Solution 1
+function sleepIn(weekday, vacation): number {
+    if(vacation==true){
+        return true;
+    }else{
+        if(weekday==true){
+            return false;
+        }else{
+            return true;
+        }
+    }   
+}
+
+Solution 2
+function sleepIn(weekday, vacation): number {
+    if(vacation || !weekday){
+        return true;
+    }
+    return false;
+}
+*/
+
+function sleepIn(weekday, vacation): number {
+    if(vacation || !weekday){
+        return true;
+    }
+    return false;
+}
